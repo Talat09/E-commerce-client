06a098af67b7439df7798e19bf48606a4aab5be2
Talat09 <talat181008312@gmail.com>
2023-09-18 14:37:09 +0600

    done contact and form submission using fromsfree

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import styled from "styled-components";
 
 const Contact = () => {
@@ -35,7 +36,49 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <h1>Contact</h1>
+      <h2 className="common-heading">Contact</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.237523657485!2d91.7919789111208!3d22.344658541200737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8df08ea1e1d%3A0xfc71b0c25f58235!2sBow%20Bazar%2C%20Chittagong!5e0!3m2!1sen!2sbd!4v1695024524785!5m2!1sen!2sbd"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xleylpna"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"
+            ></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
     </Wrapper>
   );
 };
