a64c8966dcc0ca87ed7be5c93b62ed85d967a096
Talat09 <talat181008312@gmail.com>
2023-09-18 16:56:44 +0600

    done set product and feature product using useReducer

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,24 +1,36 @@
 //create a context
 //provider
 //consumer => useContext Hook
-
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import { createContext, useContext, useEffect } from "react";
-const API = "https://api.pujakaitem.com/api/products";
+import reducer from "../reducer/productReducer";
 const AppContext = createContext();
+const API = "https://api.pujakaitem.com/api/products";
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const getProducts = async (url) => {
-    const res = await axios.get(url);
-    const products = await res.data;
-    console.log(products);
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      console.log("products", products);
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch {
+      dispatch({ type: "API_ERROR" });
+    }
   };
   useEffect(() => {
     getProducts(API);
   }, []);
   return (
-    <AppContext.Provider value={{ name: "Talat" }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 //custom hooks
