dc28e47336f7d01a572d46ae3996d16c63dd8d57
Talat09 <talat181008312@gmail.com>
2023-09-21 03:28:35 +0600

    done grid layout and show data as grid view

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useFilterContext } from "../../context/filterContext";
+import GridView from "./GridView";
+// import ListView from "./ListView";
 const ProductList = () => {
-  return <div>Product List</div>;
+  const { filter_products, grid_view } = useFilterContext();
+  if (grid_view === true) {
+    return <GridView products={filter_products} />;
+  }
+  //   if (setGridView === false) {
+  //     return <ListView products={filter_products} />;
+  //   }
 };
 
 export default ProductList;
